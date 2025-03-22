9dae7a6f745a60a0f13172d82ccd07d7e657b7c9
Bruno Caldas <brunopaivacaldas@hotmail.com>
2025-03-22 18:58:18 -0300

    retirada de código comentado.

--- src/main/java/br/com/digio/adega/repository/CompraRepository.java
+++ src/main/java/br/com/digio/adega/repository/CompraRepository.java
@@ -6,16 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CompraRepository extends JpaRepository<Compra, Integer> {
-
-//    @Query("""
-//        SELECT new br.com.digio.adega.domain.dto.CompraDTO(
-//            new br.com.digio.adega.domain.dto.ClienteDTO(c.cliente.nome, c.cliente.cpf),
-//            new br.com.digio.adega.domain.dto.ProdutoDTO(c.produto.tipoVinho, c.produto.preco, c.produto.safra, c.produto.anoCompra),
-//            c.quantidade,
-//            c.valorTotal
-//        )
-//        FROM Compra c
-//        ORDER BY c.valorTotal ASC
-//    """)
     List<Compra> findAllByOrderByValorTotalAsc();
 }
